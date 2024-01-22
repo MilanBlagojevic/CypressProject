8ceda9e0544e6a63874be95feec249c97b3974e2
MilanBlagojevic <milan.blagojevic.90@gmail.com>
2024-01-22 15:34:19 +0100

    TC14_womenTops_final

---
+++ cypress/e2e/TestCases/TC14_WomenTops_Final.cy.js
@@ -0,0 +1,52 @@
+/// <reference types="cypress" />
+
+Cypress.on("uncaught:exception", (err, runnable) => {
+    return false;
+  });
+
+import HomePageActions from "../../PageObjects/PageActions/HomePageActions.cy";
+
+describe("TC11 - Women_Hoodies", () => {
+  const homePage = new HomePageActions();
+
+  beforeEach(() => {
+    homePage.navigateToURL();
+  });
+
+  it("Validate Title of the Page", () => {
+    homePage.validateTitle().should("eq", "Home Page");
+  });
+
+  it("Add to Cart functionality", () => {
+    cy.contains('Women').trigger('mouseover', {force: true});
+    cy.contains('Tops').trigger('mouseover', {force: true});
+    cy.contains('Bras & Tanks').click({force: true});
+    cy.get(':nth-child(4) > .toolbar-sorter > #sorter').select('Price');
+
+    cy.get(':nth-child(3) > .product-item-info > .details > .name > .product-item-link').click();
+    cy.get('#product-price-1700 > .price').contains('$29.00');
+    cy.get('.stock > span').contains('In stock');
+    cy.get('#option-label-size-143-item-170').click();
+    cy.get('#option-label-color-93-item-56').click();
+    cy.get('#qty').clear().type('5');
+    cy.get('#product-addtocart-button').click();
+
+    cy.wait(2000);
+
+    cy.get('.message-success > div > a').click();
+
+    cy.wait(4000);
+
+    cy.get('.checkout-methods-items > :nth-child(1) > .action > span').click();
+
+    cy.wait(5000);
+
+    cy.get('#customer-email-fieldset > .required > .control > #customer-email').type('milan.testing.978@email.com');
+    cy.xpath("//input[@class='input-text' and @type='text' and @name='firstname']").type('Milan');
+    cy.xpath("//input[@class='input-text' and @type='text' and @name='lastname']").type('Testing');
+    cy.xpath("//input[@class='input-text' and @type='text' and @name='company']").type('Testing Inc');
+    cy.xpath("//input[@class='input-text' and @type='text' and @name='street[0]']").type('Testerska 17');
+    cy.xpath("//input[@class='input-text' and @type='text' and @name='city']").type('Novi Sad');
+    cy.xpath("//select[@class='select' and @name='region_id']").select('Florida');
+  });
+});
